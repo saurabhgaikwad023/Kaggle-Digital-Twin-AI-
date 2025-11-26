83bee6f3239a4719c67ca884094b2e0b1e87fa03
Saurabh Gaikwad <73156259+saurabhgaikwad023@users.noreply.github.com>
2025-11-26 23:20:20 +0530

    Update memory.py

--- agents/memory.py
+++ agents/memory.py
@@ -1,10 +1,9 @@
-# agents/memory.py
 class MemoryBank:
     def __init__(self):
-        self.store = {}
-    def save_user(self, user_id, data):
-        self.store[user_id] = data
-    def get_user(self, user_id):
-        return self.store.get(user_id, {})
-    def log_episode(self, user_id, episode_id, result):
-        self.store.setdefault(user_id, {}).setdefault('episodes', {})[episode_id] = result
+        self.store={}
+    def save_user(self,user,data):
+        self.store[user]=data
+    def get_user(self,user):
+        return self.store.get(user,{})
+    def log_episode(self,user,episode,res):
+        self.store.setdefault(user,{}).setdefault("episodes",{})[episode]=res
