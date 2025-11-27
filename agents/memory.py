6af49c8e8ae97b58017f1a422c9b518ac6719741
Saurabh Gaikwad <73156259+saurabhgaikwad023@users.noreply.github.com>
2025-11-27 22:28:48 +0530

    Delete agents/memory.py

--- agents/memory.py
+++
@@ -1,9 +0,0 @@
-class MemoryBank:
-    def __init__(self):
-        self.store={}
-    def save_user(self,user,data):
-        self.store[user]=data
-    def get_user(self,user):
-        return self.store.get(user,{})
-    def log_episode(self,user,episode,res):
-        self.store.setdefault(user,{}).setdefault("episodes",{})[episode]=res
